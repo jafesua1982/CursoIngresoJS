1ae9e43a8c94d90bf1f0063d738ab4923d27824e
jafesua1982 <47691359+jafesua1982@users.noreply.github.com>
2019-07-01 22:03:59 -0300

    Add files via upload
    ejercicios resueltos de clase 1

--- 1-EntradaSalida/jsEntradaSalida-5.js
+++ 1-EntradaSalida/jsEntradaSalida-5.js
@@ -2,6 +2,11 @@
 ej.: "Usted se llama José y tiene 66 años" 	*/
 function mostrar()
 {	
-	
+var nombre;
+var edad;
+nombre = document.getElementById("el nombre").value;
+edad = document.getElementById("laEdad").value;
+
+alert(" Ud se llama " + nombre + " y tiene " + edad + " años ");
 }
 
